2fea5f531b20cce631be5938a64efe4d1d663cc7
muh.firdaus <61868483+mfierd@users.noreply.github.com>
2020-12-16 20:55:44 +0700

    Add files via upload

---
+++ WithOCP/CouponCashback.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CouponWithOCP
+{
+    class CouponCashback : Coupon
+    {
+        public double discNominal;
+
+        public CouponCashback(double discNominal)
+        {
+            this.discNominal = discNominal;
+        }
+
+        public override double calculate(double originPrice)
+        {
+            return originPrice - discNominal;
+        }
+
+    }
+}
